f51d8280e1040b2aa2b2a01c1f2af5688183fa4a
Paweł Sawicki <psawicki97@gmail.com>
2019-06-11 23:22:51 +0200

    add auth

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,8 @@
 import {Component} from '@angular/core';
+import {NgForm} from '@angular/forms';
+import {AuthService, AuthResponseData} from './auth.service';
+import {error} from '@angular/compiler/src/util';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-auth',
@@ -6,8 +10,46 @@ import {Component} from '@angular/core';
 })
 export class AuthComponent {
   isLoginMode = true;
+  isLoading = false;
+  error: string = null;
+
+  constructor(private authService: AuthService) {
+
+  }
 
   onswitchMode() {
     this.isLoginMode = !this.isLoginMode;
+  }
+
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+
+    const email = form.value.email;
+    const password = form.value.password;
+
+    let authObs: Observable<AuthResponseData>;
+
+    this.isLoading = true;
+    if (this.isLoginMode) {
+     authObs = this.authService.login(email, password);
+    } else {
+      authObs = this.authService.signup(email, password);
+    }
+
+    authObs.subscribe(
+      resData => {
+        console.log(resData);
+        this.isLoading = false;
+        this.error = null;
+      },
+      errorMessage => {
+        console.log(errorMessage);
+        this.error = errorMessage;
+        this.isLoading = false;
+      }
+    )
+    form.reset();
   }
 }
---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,57 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {catchError} from 'rxjs/operators';
+import {throwError} from 'rxjs';
+import {error} from '@angular/compiler/src/util';
+
+export interface AuthResponseData {
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: boolean;
+}
+
+@Injectable({providedIn: 'root'})
+export class AuthService {
+  constructor(private http: HttpClient) {
+  }
+
+  signup(email: string, password: string) {
+    return this.http.post<AuthResponseData>(
+      'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyClj8yc26sp_t1RKg9yu16Xn8zkfHSBFcs',
+      {
+        email: email,
+        password: password,
+        returnSecureToken: true
+      }
+    ).pipe(catchError(this.hadleError));
+  }
+
+  login(email: string, password: string) {
+    return this.http.post<AuthResponseData>(
+      'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyClj8yc26sp_t1RKg9yu16Xn8zkfHSBFcs',
+      {
+        email: email,
+        password: password,
+        returnSecureToken: true
+      }
+    ).pipe(catchError(this.hadleError));
+  }
+
+  private hadleError(errorRes: HttpErrorResponse) {
+        let errorMessage = 'An unknown error occured!';
+        if (!errorRes.error || !errorRes.error.error) {
+          return throwError(errorMessage);
+        }
+        switch (errorRes.error.error.message) {
+          case 'EMAIL_EXISTS':
+            errorMessage = 'this email exissts already';
+            break;
+        }
+        return throwError(errorMessage);
+  }
+
+}
