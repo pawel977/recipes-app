785a222b775d1370041d697d4de3474b7ca9d548
Paweł Sawicki <psawicki97@gmail.com>
2019-06-18 09:13:53 +0200

    init dynamic alert modal

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,9 +1,11 @@
-import {Component} from '@angular/core';
+import {Component, ComponentFactoryResolver, ViewChild} from '@angular/core';
 import {NgForm} from '@angular/forms';
 import {AuthService, AuthResponseData} from './auth.service';
 import {error} from '@angular/compiler/src/util';
 import {Observable} from 'rxjs';
 import {Router} from '@angular/router';
+import {AlertComponent} from '../shared/alert/alert.component';
+import {PlaceholderDirective} from '../shared/placeholder/placeholder.directive';
 
 @Component({
   selector: 'app-auth',
@@ -13,8 +15,9 @@ export class AuthComponent {
   isLoginMode = true;
   isLoading = false;
   error: string = null;
+  @ViewChild(PlaceholderDirective, { static: false } ) alertHost: PlaceholderDirective;
 
-  constructor(private authService: AuthService, private  router: Router) {
+  constructor(private authService: AuthService, private  router: Router, private componentFactoryResolver: ComponentFactoryResolver) {
 
   }
 
@@ -48,9 +51,21 @@ export class AuthComponent {
       errorMessage => {
         console.log(errorMessage);
         this.error = errorMessage;
+        this.showErrorAlert(errorMessage);
         this.isLoading = false;
       }
     )
     form.reset();
+  }
+
+  onHandleError() {
+    this.error = null;
+  }
+
+  private showErrorAlert(message: string) {
+    const alertComponentFactory = this.componentFactoryResolver.resolveComponentFactory(AlertComponent);
+    const hostViewContainerRef = this.alertHost.viewContainerRef;
+    hostViewContainerRef.clear();
+    hostViewContainerRef.createComponent(alertComponentFactory);
   }
 }
